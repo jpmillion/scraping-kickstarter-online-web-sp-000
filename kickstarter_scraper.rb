a3b1203b17624d0f50deb41f2ff01638a4d15fbf
John Million <jpmillion@comcast.net>
2020-11-20 17:46:34 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,7 +13,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   projects = {}
   kickstarter.css("li.project.grid_4").each do |project|
-    title = 
+    title = project.css()
   end
   projects
 end
